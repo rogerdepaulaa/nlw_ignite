669e707eda81b7abe374a5f60d92c9c1dbfb5488
rogerdepaulaa <roger.philippe.18@gmail.com>
2022-09-12 23:35:46 -0300

    Landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,80 @@
-interface ButtonProps{
-  title: string;
-}
-
-function Button(props: ButtonProps){
-  return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
+import {MagnifyingGlassPlus} from 'phosphor-react';
+import  './styles/main.css';
+import logoImg from './assests/logo-nlw-esports.svg';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center m-20'>
+      <img src={logoImg} alt="" />
+
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
+      </h1>
+      <div className='grid grid-cols-6 gap-6 mt-16 '>
+        
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-1.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 block'>4 Anúncio</span>
+          </div>
+        </a>
+        
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-2.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Dota 2</strong>
+            <span className='text-zinc-300 block'>4 Anúncio</span>
+          </div>
+        </a>
+        
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-3.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Counter Strike</strong>
+            <span className='text-zinc-300 block'>4 Anúncio</span>
+          </div>
+        </a>
+        
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-4.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Apex</strong>
+            <span className='text-zinc-300 block'>4 Anúncio</span>
+          </div>
+        </a>
+        
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-5.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Fortnite</strong>
+            <span className='text-zinc-300 block'>4 Anúncio</span>
+          </div>
+        </a>
+        
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-6.png" alt="" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>World of Warcraft</strong>
+            <span className='text-zinc-300 block'>4 Anúncio</span>
+          </div>
+        </a>
+      </div>
+
+      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8'>
+        <div className='bg-[#2A2634] px-8 py-6  rounded-lg flex justify-between items-center '>
+          <div>
+            <strong className='text-white font-black text-2xl block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 text-base'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+          <button className='py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3' >
+            <MagnifyingGlassPlus size={24} />
+            Publicar Anúncio
+
+          </button>
+
+        </div>
+      </div>
 
     </div>
   )
