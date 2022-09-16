3464424d8a239648de285e490c035eec90de0b8f
rogerdepaulaa <roger.philippe.18@gmail.com>
2022-09-16 15:25:31 -0300

    server

---
+++ server/src/utils/convert-hour-string-to-minutes.ts
@@ -0,0 +1,7 @@
+// 18:00 -> 1080
+export function convertHourStringToMinute(hourString: string) {
+  const [hours, minutes] = hourString.split(":").map(Number);
+
+  const minutesAmount = hours * 60 + minutes;
+  return minutesAmount;
+}
